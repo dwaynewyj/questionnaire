e55ccd11ed4b388f10f12bfd600fcab612af69ae
dwaynewyj <167665968+dwaynewyj@users.noreply.github.com>
2024-07-08 14:08:29 -0700

    Update app.component.spec.ts

--- fe/src/app/app.component.spec.ts
+++ fe/src/app/app.component.spec.ts
@@ -7,7 +7,6 @@ import { RouterTestingModule } from '@angular/router/testing';
 
 import { AppComponent } from './app.component';
 
-
 describe('AppComponent', () => {
   let component: AppComponent;
   let fixture: ComponentFixture<AppComponent>;
